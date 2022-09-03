2ceb6849741356a9173ca9257c5d55eb080c8143
Eugene Kutas <evgenij.kutas@gmail.com>
2022-09-04 00:11:58 +0300

    Envoy config updated

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -8,5 +8,6 @@ composer install --no-dev -o --prefer-dist
 php artisan migrate --force
 php artisan optimize:clear
 php artisan queue:restart
+php artisan websockets:restart
 php artisan up
 @endtask
